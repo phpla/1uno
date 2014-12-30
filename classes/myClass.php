03d63c8bd41d57bfc48936546050e6f1e453fe82
dec31 <test@gmail.com>
2014-12-31 00:12:29 +1300

    nueva clase

---
+++ classes/myClass.php
@@ -0,0 +1,15 @@
+class myClass {
+	const VERSION = '4.5.4';
+
+	public $variable1;
+	public $variable2;
+
+	protected $error;
+
+	public function __construct(){
+		
+		$error = NULL;
+		$variable1 = $variable2 = NULL;
+	}
+
+ }	
